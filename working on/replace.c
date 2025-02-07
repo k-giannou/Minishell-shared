3f4f572a850bbf323bf6a2046844290df7a098eb
kgiannou <kgiannou@student.42perpignan.fr>
2025-02-07 20:16:53 +0100

    replace2

--- working on/replace.c
+++ working on/replace.c
@@ -10,21 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char  *replace_var(t_minishell *mini, char *str)//str=$user
+char	*replace_var(t_minishell *mini, char *str)//str=$user
 {
         t_env     *current;
+		char	*dest;
+		int	len;
+		int	i;
 	
         current = mini->env;
         if (!current)
 			return NULL;
+		len = 0;
+		i = 0;
 		while (current)
         {
 				if (ft_strncmp(str, current->data, strlen(str)) == 0)
-				{		
-					//printf("%s\n", current->data + (strlen(str) + 1));
-					return (current->data + (strlen(str) + 1));
+				{
+					len = strlen(current->data);
+					len = len - strlen(str) + 1;
+					dest = malloc(sizeof(char)* len + 1);
+					if (!dest)
+						return (NULL);
+					len = strlen(str) + 1;
+					while(current->data[i + len] != '\0')
+					{
+						dest[i] = current->data[i + len];
+						i++;
+					}
+							
+					//printf("%s\n", dest);
+					return (dest);
 				}
                 current = current->next;
         }
 		return (str);
 }
+

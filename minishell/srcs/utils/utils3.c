742a8213bb797a3fd2e03b2541e803600a74f7c8
kgiannou <kgiannou@student.42perpignan.fr>
2025-02-28 12:24:16 +0100

    update

--- minishell/srcs/utils/utils3.c
+++ minishell/srcs/utils/utils3.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils3.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: locagnio <locagnio@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kgiannou <kgiannou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 16:45:29 by locagnio          #+#    #+#             */
-/*   Updated: 2025/02/26 20:07:24 by locagnio         ###   ########.fr       */
+/*   Updated: 2025/02/28 12:23:21 by kgiannou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@ char	*host_dup(char *name)
 	char	*dest;
 	int		i;
 	int		k;
+	unsigned char unicode_flower[] = {0xF0, 0x9F, 0x8C, 0xB8};
 
 	i = ft_strlen(name);
-	dest = (char *)ft_calloc(sizeof(char), i + 2);
+	dest = (char *)ft_calloc(sizeof(char), i + 20);//i + 2
 	if (!dest)
 		return (NULL);
 	i = 0;
-	k = 1;
-	dest[0] = '@';
+	//k = 1;
+	k = 0;
+	for (int j = 0; j < 4; j++)
+        dest[k++] = unicode_flower[j];
+	//dest[0] = '@';
 	while (name[i] != '.' && name[i] != '\0')
 		dest[k++] = name[i++];
 	dest[k++] = ':';

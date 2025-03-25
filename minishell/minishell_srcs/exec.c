73b0e9f631e231acb21d09960e051efa96fab610
Louis Cagnion <locagnio@c1r1p6.42perpignan.fr>
2025-03-25 18:07:32 +0100

    minishell update

--- minishell/minishell_srcs/exec.c
+++ minishell/minishell_srcs/exec.c
@@ -6,7 +6,7 @@
 /*   By: locagnio <locagnio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 16:33:34 by locagnio          #+#    #+#             */
-/*   Updated: 2025/03/25 17:56:25 by locagnio         ###   ########.fr       */
+/*   Updated: 2025/03/25 18:06:47 by locagnio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,23 @@
 
 void	exec_cmd(t_minishell *mini)
 {
-	if (!ft_strcmp(mini->tokens[0], "exit") && mini->p.nb_pipes == 0)
-		ft_exit(mini);
+	if (is_buildin(mini->tokens[0], 0)&& mini->p.nb_pipes == 0)
+	{
+		if (!ft_strcmp(mini->tokens[0], "exit"))
+			ft_exit(mini);
+		else if (!ft_strcmp(mini->tokens[0], "pwd"))
+			pwd();
+		else if (!ft_strcmp(mini->tokens[0], "echo"))
+			echo(mini->tokens);
+		else if (!ft_strcmp(mini->tokens[0], "cd"))
+			cd(mini->tokens, &mini);
+		else if (!ft_strcmp(mini->tokens[0], "export"))
+			export(mini->tokens, mini);
+		else if (!ft_strcmp(mini->tokens[0], "unset"))
+			unset(mini->tokens, mini);
+		else if (!ft_strcmp(mini->tokens[0], "env"))
+			ft_env(mini->env);
+	}
 	else
 		pipex(mini, splited_env(mini->env));
 	free_all(mini, "tabs");

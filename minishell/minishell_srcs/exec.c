85aa73baf1eac1e592c93846d345b345bcf3bf70
Louis Cagnion <locagnio@c1r1p6.42perpignan.fr>
2025-03-25 17:55:40 +0100

    minishell update

--- minishell/minishell_srcs/exec.c
+++ minishell/minishell_srcs/exec.c
@@ -6,7 +6,7 @@
 /*   By: locagnio <locagnio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 16:33:34 by locagnio          #+#    #+#             */
-/*   Updated: 2025/03/23 17:06:47 by locagnio         ###   ########.fr       */
+/*   Updated: 2025/03/25 17:54:51 by locagnio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 void	exec_cmd(t_minishell *mini)
 {
-	pipex(mini, splited_env(mini->env));
+	if (!ft_strcmp(mini->tokens[0], "exit"))
+		ft_exit(mini);
+	else
+		pipex(mini, splited_env(mini->env));
 	free_all(mini, "tabs");
 	mini->tokens = NULL;
 	mini->pipes_redirs = NULL;
